a4b95fb3d88396621a73fd5e61dfcda8a3aab801
WandersonGomes <dersom100@gmail.com>
2021-01-05 12:06:35 -0300

    Comparison

--- capitulo02/capitulo02/src/main/java/com/wanderson/capitulo02/Comparison.java
+++ capitulo02/capitulo02/src/main/java/com/wanderson/capitulo02/Comparison.java
@@ -35,5 +35,7 @@ public class Comparison {
             System.out.printf("%d <= %d%n", number1, number2);
         if (number1 >= number2)
             System.out.printf("%d >= %d%n", number1, number2);
+        
+        input.close();
     }
 }
