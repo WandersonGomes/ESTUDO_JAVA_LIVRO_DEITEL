dca94515ef5ebefe537bdb4869071372541f840a
WandersonGomes <dersom100@gmail.com>
2021-01-05 11:58:42 -0300

    Comparison

---
+++ capitulo02/capitulo02/src/main/java/com/wanderson/capitulo02/Comparison.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wanderson.capitulo02;
+
+import java.util.Scanner;
+/**
+ *
+ * @author lobonegro
+ */
+public class Comparison {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int number1;
+        int number2;
+        
+        System.out.println("Enter first integer: ");
+        number1 = input.nextInt();
+        
+        System.out.println("Enter second integer: ");
+        number2 = input.nextInt();
+        
+        if (number1 == number2)
+            System.out.printf("%d == %d%n", number1, number2);
+        if (number1 != number2)
+            System.out.printf("%d != %d%n", number1, number2);
+        if (number1 < number2)
+            System.out.printf("%d < %d%n", number1, number2);
+        if (number1 > number2)
+            System.out.printf("%d > %d%n", number1, number2);
+        if (number1 <= number2)
+            System.out.printf("%d <= %d%n", number1, number2);
+        if (number1 >= number2)
+            System.out.printf("%d >= %d%n", number1, number2);
+    }
+}
