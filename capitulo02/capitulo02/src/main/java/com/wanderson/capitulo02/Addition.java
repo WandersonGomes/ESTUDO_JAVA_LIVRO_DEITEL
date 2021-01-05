dcf72202e83188879515d2a41b448d493633fb56
WandersonGomes <dersom100@gmail.com>
2021-01-05 00:07:15 -0300

    Addition

---
+++ capitulo02/capitulo02/src/main/java/com/wanderson/capitulo02/Addition.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wanderson.capitulo02;
+
+import java.util.Scanner;
+/**
+ *
+ * @author lobonegro
+ */
+public class Addition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int number1;
+        int number2;
+        int sum;
+        
+        System.out.println("Enter first integer: ");
+        number1 = input.nextInt();
+        
+        System.out.println("Enter second integer: ");
+        number2 = input.nextInt();
+        
+        sum = number1 + number2;
+        
+        System.out.printf("Sum is %d%n", sum);
+    }
+}
