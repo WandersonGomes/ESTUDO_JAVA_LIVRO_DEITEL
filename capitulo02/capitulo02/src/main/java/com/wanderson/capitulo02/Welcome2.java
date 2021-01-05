9b35bf10cacd6a106201cce830b448fb23e0f9ef
WandersonGomes <dersom100@gmail.com>
2021-01-04 23:54:27 -0300

    Welcome2

---
+++ capitulo02/capitulo02/src/main/java/com/wanderson/capitulo02/Welcome2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wanderson.capitulo02;
+
+/**
+ *
+ * @author lobonegro
+ */
+public class Welcome2 {
+    public static void main(String[] args) {
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+    }
+}
