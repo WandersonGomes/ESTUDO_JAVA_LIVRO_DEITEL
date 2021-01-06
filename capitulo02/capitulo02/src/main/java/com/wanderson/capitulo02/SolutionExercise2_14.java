46518df3ebcb55b9f81aa1278efed5d0e1e24108
WandersonGomes <dersom100@gmail.com>
2021-01-06 00:57:00 -0300

    SolutionExercise2_14

---
+++ capitulo02/capitulo02/src/main/java/com/wanderson/capitulo02/SolutionExercise2_14.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lobonegro
+ */
+public class SolutionExercise2_14 {
+    public static void main(String[] args) {
+        //item a
+        System.out.println("1 2 3 4");
+        
+        //item b
+        System.out.print("1");
+        System.out.print(" 2");
+        System.out.print(" 3");
+        System.out.print(" 4");
+        
+        //item c
+        System.out.printf("%n%d %d %d %d%n", 1, 2, 3, 4);
+        //System.out.printf("%n1 2 3 4%n");
+    }
+}
